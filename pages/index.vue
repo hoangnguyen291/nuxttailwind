b8bb3db7aa7843f17e14e97bfa6aa13bf413bf92
hoangnguyen291 <hoangnguyen291@hotmail.com>
2020-11-10 22:03:29 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tw-bg-gray-200 tw-pt-5 tw-min-h-screen">
+    <div class="tw-container tw-mx-auto">
+      <div class="tw-grid tw-grid-cols-12 tw-gap-5">
+        <div class="tw-col-span-5">
+          <div class="tw-bg-white tw-rounded-xl tw-shadow-sm tw-p-4">
+            <p class="tw-text-dark-500 tw-font-bold tw-text-xl tw-m-0">
+              Giới thiệu
+            </p>
+            <button
+              class="tw-w-full tw-mt-4 tw-bg-gray-400 tw-block tw-outline-none tw-text-dark-500 tw-font-semibold tw-p-2 tw-rounded-lg"
+            >
+              Chỉnh sửa chi tiết
+            </button>
+            <button
+              class="tw-w-full tw-mt-4 tw-bg-gray-400 tw-block tw-outline-none tw-text-dark-500 tw-font-semibold tw-p-2 tw-rounded-lg"
+            >
+              Thêm sở thích
+            </button>
+            <button
+              class="tw-w-full tw-mt-4 tw-bg-gray-400 tw-block tw-outline-none tw-text-dark-500 tw-font-semibold tw-p-2 tw-rounded-lg"
+            >
+              Chỉnh sửa phần đăng ký
+            </button>
+          </div>
+        </div>
+        <div class="tw-col-span-7">
+          <div class="tw-bg-white tw-p-4 tw-rounded-xl tw-shadow-sm">
+            <div class="tw-flex">
+              <img
+                width="50"
+                class="tw-rounded-full"
+                src="https://photo2.tinhte.vn/data/avatars/m/1470/1470679.jpg?1443582320"
+                alt=""
+              />
+              <input
+                class="tw-ml-2 tw-rounded-full tw-bg-gray-200 tw-p-2 tw-w-full tw-outline-none"
+                type="text"
+                placeholder="Bạn đang nghĩ gì?"
+              />
+            </div>
+            <hr class="tw-my-3" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
